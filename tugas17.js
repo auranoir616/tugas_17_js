97fdd91dc1fa79993c1c253253ac5b86eb9d9b6f
febri <fitrafilthmongers@gmail.com>
2023-07-19 13:01:31 +0700

    tugas 17 js
    mengumpulkan tugas 17 js

---
+++ tugas17.js
@@ -0,0 +1,10 @@
+var soal1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+var soal2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual. ";
+console.log(soal1.toLocaleLowerCase())
+console.log(soal1.toLocaleUpperCase())
+console.log(soal2.toLocaleLowerCase())
+console.log(soal2.toLocaleUpperCase())
+
+
+var soal3 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+console.log(soal3.charCodeAt(0), soal3.charCodeAt(1), soal3.charCodeAt(2), soal3.charCodeAt(3), soal3.charCodeAt(4), soal3.charCodeAt(5))
